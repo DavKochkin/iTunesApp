b07bf1abce7ef09d0c097580afda4582737ab558
David Kochkin <kochkin.david@gmail.com>
2023-08-26 11:35:32 +0300

    User Authorization

--- iTunesApp/Controllers/AuthViewController.swift
+++ iTunesApp/Controllers/AuthViewController.swift
@@ -116,9 +116,35 @@ class AuthViewController: UIViewController {
     }
     
     @objc func signInButtonTapped() {
-        let navVC = UINavigationController(rootViewController: AlbumsViewController())
-        navVC.modalPresentationStyle = .fullScreen
-        self.present(navVC, animated: true)
+        
+        
+        let mail = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let user = findUserDataBase(mail: mail)
+        
+        if user == nil {
+            loginLabel.text = "User not found"
+            loginLabel.textColor = .red
+        } else if user?.password == password {
+            let navVC = UINavigationController(rootViewController: AlbumsViewController())
+            navVC.modalPresentationStyle = .fullScreen
+            self.present(navVC, animated: true)
+        } else {
+            loginLabel.text = "Wrong password"
+            loginLabel.textColor = .red
+        }
+    }
+    
+    private func findUserDataBase(mail: String) -> User? {
+        let dataBase = DataBase.shared.users
+        print(dataBase)
+        
+        for user in dataBase {
+            if user.email == mail {
+                return user
+            }
+        }
+        return nil
     }
 }
 
