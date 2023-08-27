e8ac98db253b4831e1d6ffa1f098b332d232a52f
David Kochkin <kochkin.david@gmail.com>
2023-08-27 09:28:36 +0300

    Add ActiveUser

--- iTunesApp/Services/UserDefaultsManager.swift
+++ iTunesApp/Services/UserDefaultsManager.swift
@@ -13,10 +13,12 @@ class DataBase {
     
     enum SettingKey: String {
         case users
+        case activeUser
     }
     
     let defaults = UserDefaults.standard
     let userKey = SettingKey.users.rawValue
+    let activeUserKey = SettingKey.activeUser.rawValue
     
     var users: [User] {
         get {
@@ -37,5 +39,24 @@ class DataBase {
         
         let user = User(firstName: firstName, secondName: secondName, phone: phone, email: email, password: password, age: age)
         users.insert(user, at: 0)
+    }
+    
+    var activeUser: User? {
+        get {
+            if let data = defaults.value(forKey: activeUserKey) as? Data {
+                return try! PropertyListDecoder().decode(User.self, from: data)
+            } else {
+                return nil
+            }
+        }
+        set {
+            if let data = try? PropertyListEncoder().encode(newValue) {
+                defaults.set(data, forKey: activeUserKey)
+            }
+        }
+    }
+
+    func saveActiveUser(user: User) {
+        activeUser = user
     }
 }
