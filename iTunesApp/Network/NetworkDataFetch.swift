1dfef7eeb88490314ec3098ed3fb563dbee688d4
David Kochkin <kochkin.david@gmail.com>
2023-08-28 12:18:21 +0300

    Add NetworkDataFetch

---
+++ iTunesApp/Network/NetworkDataFetch.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkDataFetch.swift
+//  iTunesApp
+//
+//  Created by David Kochkin on 28.08.2023.
+//
+
+import UIKit
+
+class NetworkDataFetch {
+     
+    static let shared = NetworkDataFetch()
+    
+    private init() {}
+    
+    func fetchAlbum(urlString: String, response: @escaping (AlbumModel?, Error?) -> Void) {
+        
+        NetworkRequest.shared.requestDate(urlString: urlString) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let albums = try JSONDecoder().decode(AlbumModel.self, from: data)
+                    response(albums, nil)
+                } catch let jsonError {
+                    print("Fail to decode JSON", jsonError)
+                }
+            case .failure(let error):
+                print("Error received requesting data: \(error.localizedDescription)")
+                response(nil, error)
+            }
+        }
+    }
+}
